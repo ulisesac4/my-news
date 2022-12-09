5059e5ac5988224d0bc1ba98f3d2522f3ae509f0
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-09 17:45:32 -0600

    add basic migrations and model definitions

---
+++ backend/models/template.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Template extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Template.init({
+    name: DataTypes.STRING,
+    content: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Template',
+  });
+  return Template;
+};
