bcb369e651b5b368d42e1d21db31972601fcb755
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-08 23:09:58 -0600

    add the basic server file

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config({ override: true });
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Express Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
