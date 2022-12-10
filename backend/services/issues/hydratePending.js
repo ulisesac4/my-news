083d84a864958150722aa26b82df5f3739147052
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-10 12:28:38 -0600

    add time comparison to know if date of the issue has passed to send it right away

--- backend/services/issues/hydratePending.js
+++ backend/services/issues/hydratePending.js
@@ -1,4 +1,5 @@
 const schedule = require("node-schedule");
+const { isAfter } = require("date-fns");
 const Models = require("../../models");
 const getUnsentIssues = require("./getUnsentIssues");
 const send = require("./send");
@@ -19,9 +20,14 @@ module.exports = async () => {
   unsentIssues.forEach(async (unsentIssue) => {
     try {
       const date = new Date(moment(unsentIssue.publishDate).toISOString());
-      const job = schedule.scheduleJob(date, async () => {
+
+      if (isAfter(date, new Date())) {
+        const job = schedule.scheduleJob(date, async () => {
+          await send(unsentIssue.id);
+        });
+      } else {
         await send(unsentIssue.id);
-      });
+      }
     } catch (error) {
       console.error("[Err] Error at hydrating pending issue", error);
     }
