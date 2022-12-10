a41b3109a1cb9125369b25c043d8d5e23e68567e
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-10 09:07:12 -0600

    add stub for hydrate non send newsletters

--- backend/services/issues/index.js
+++ backend/services/issues/index.js
@@ -1,9 +1,17 @@
 const create = require("./create");
 const destroy = require("./destroy");
-const hidratePending = require("./hydratePending");
+const hydratePending = require("./hydratePending");
 const send = require("./send");
 const show = require("./show");
 const showAll = require("./showAll");
 const update = require("./update");
 
-module.exports = { create, destroy, send, show, showAll, update };
+module.exports = {
+  create,
+  destroy,
+  hydratePending,
+  send,
+  show,
+  showAll,
+  update,
+};
