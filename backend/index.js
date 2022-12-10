9f70ff0472bd6363866c38101f50da905dd027c7
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-09 22:53:20 -0600

    add cors and body parser middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 require("dotenv").config({ override: true });
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const express = require("express");
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors());
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 app.use("/apiv1", require("./routes"));
 
