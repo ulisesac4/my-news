461f2bfd7d93601677615dd39b70b757f4a3e277
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-09 23:57:33 -0600

    add routes for issues of newsletter

--- backend/routes/issues.js
+++ backend/routes/issues.js
@@ -4,5 +4,9 @@ const Controllers = require("../controllers");
 const router = express.Router();
 
 router.post("/", Controllers.Issues.create);
+router.delete("/", Controllers.Issues.destroy);
+router.get("/:id", Controllers.Issues.show);
+router.get("/:newsletterId", Controllers.Issues.showAll);
+router.patch("/", Controllers.Issues.update);
 
 module.exports = router;
