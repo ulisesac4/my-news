9b289a738e0e410459b346c090844783e5d8350d
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-09 23:28:14 -0600

    add basic recipient controllers and routes

--- backend/controllers/recipients.js
+++ backend/controllers/recipients.js
@@ -1,3 +1,36 @@
+const RecipientService = require("../services/recipients");
 module.exports = {
-  create: async (req, res) => {},
+  create: async (req, res) => {
+    const { emails, newsletterId } = req.body;
+    try {
+      const recipients = await RecipientService.create(emails, newsletterId);
+      res.json({ recipients });
+    } catch (error) {
+      console.error("[Error] create recipient", error);
+      res.status(400);
+      res.send(error.message);
+    }
+  },
+  destroy: async (req, res) => {
+    const { id } = req.body;
+    try {
+      const rowsDeleted = await RecipientService.destroy(id);
+      res.json({ rowsDeleted });
+    } catch (error) {
+      console.error("[Error] destroy recipient", error);
+      res.status(400);
+      res.send(error.message);
+    }
+  },
+  showAll: async (req, res) => {
+    const { newsletterId } = req.params;
+    try {
+      const recipients = await RecipientService.showAll(newsletterId);
+      res.json({ recipients });
+    } catch (error) {
+      console.error("[Error] index recipient", error);
+      res.status(400);
+      res.send(error.message);
+    }
+  },
 };
---
+++ backend/routes/recipients.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const Controllers = require("../controllers");
+
+const router = express.Router();
+
+router.post("/", Controllers.Recipients.create);
+router.delete("/", Controllers.Recipients.destroy);
+router.get("/:newsletterId", Controllers.Recipients.showAll);
+
+module.exports = router;
