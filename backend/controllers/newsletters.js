458394131e331d7665c8897456eff622a17a4008
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-10 17:36:49 -0600

    add openapi documentation for newsletter controllers

--- backend/controllers/newsletters.js
+++ backend/controllers/newsletters.js
@@ -18,6 +18,8 @@ module.exports = {
    *                 type: string
    *                 description: The Newsletter's name.
    *                 example: My-Newsletter
+   *             required:
+   *               - name
    *     responses:
    *       200:
    *         description: API has created succesfully the Newsletter
@@ -26,22 +28,32 @@ module.exports = {
    *             schema:
    *               type: object
    *               properties:
-   *                 id:
-   *                   type: integer
-   *                   description: The create Newsletter ID.
-   *                   example: 1
-   *                 name:
-   *                   type: string
-   *                   description: The Newsletter's name.
-   *                   example: My-Newsletter
-   *                 createdAt:
-   *                   type: string
-   *                   description: The Newsletter's created hour in iso format.
-   *                   example: 2022-12-06T00:53:42Z
-   *                 updatedAt:
-   *                   type: string
-   *                   description: The Newsletter's updated hour in iso format.
-   *                   example: 2022-12-06T00:53:42Z
+   *                 newsletter:
+   *                   type: object
+   *                   properties:
+   *                    id:
+   *                     type: integer
+   *                     description: The create Newsletter ID.
+   *                     example: 1
+   *                    name:
+   *                     type: string
+   *                     description: The Newsletter's name.
+   *                     example: My-Newsletter
+   *                    createdAt:
+   *                     type: string
+   *                     description: The Newsletter's created hour in iso format.
+   *                     example: 2022-12-06T00:53:42Z
+   *                    updatedAt:
+   *                     type: string
+   *                     description: The Newsletter's updated hour in iso format.
+   *                     example: 2022-12-06T00:53:42Z
+   *       400:
+   *         description: An error has ocurred
+   *         content:
+   *           text/plain; charset=utf-8:
+   *             schema:
+   *               type: string
+   *               example: name is invalid
    */
   create: async (req, res) => {
     const { name } = req.body;
@@ -60,10 +72,39 @@ module.exports = {
    *  delete:
    *     tags:
    *     - Newsletter
-   *     description: Create an Newsletter and schedules it
+   *     description: Destroy an Newsletter only, issues and recipients will remain but will become inaccesible
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               id:
+   *                 type: text
+   *                 description: The Newsletter's id.
+   *                 example: 2
+   *             required:
+   *               - id
    *     responses:
    *       200:
-   *         description: API has created succesfully the Newsletter
+   *         description: API has deleted succesfully the Newsletter
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 rowsDeleted:
+   *                   type: integer
+   *                   description: The number of newsletters updated, normally must be 1 it may be 0 if it didn't updated.
+   *                   example: 1
+   *       400:
+   *         description: An error has ocurred
+   *         content:
+   *           text/plain; charset=utf-8:
+   *             schema:
+   *               type: string
+   *               example: name is invalid
    */
   destroy: async (req, res) => {
     const { id } = req.body;
@@ -82,10 +123,43 @@ module.exports = {
    *  get:
    *     tags:
    *     - Newsletter
-   *     description: Create an Newsletter and schedules it
+   *     description: Get all Newsletters
+   *
    *     responses:
    *       200:
-   *         description: API has created succesfully the Newsletter
+   *         description: API has fetched succesfully the Newsletters
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 newsletters:
+   *                   type: array
+   *                   items:
+   *                     properties:
+   *                       id:
+   *                         type: integer
+   *                         description: The Newsletter's ID.
+   *                         example: 1
+   *                       name:
+   *                         type: string
+   *                         description: The Newsletter's name.
+   *                         example: My-Newsletter
+   *                       createdAt:
+   *                        type: string
+   *                        description: The Newsletter's created hour in iso format.
+   *                        example: 2022-12-06T00:53:42Z
+   *                       updatedAt:
+   *                        type: string
+   *                        description: The Newsletter's updated hour in iso format.
+   *                        example: 2022-12-06T00:53:42Z
+   *       400:
+   *         description: An error has ocurred
+   *         content:
+   *           text/plain; charset=utf-8:
+   *             schema:
+   *               type: string
+   *               example: name is invalid
    */
   showAll: async (req, res) => {
     try {
@@ -103,10 +177,45 @@ module.exports = {
    *  patch:
    *     tags:
    *     - Newsletter
-   *     description: Create an Newsletter and schedules it
+   *     description: Update an Newsletter
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               id:
+   *                 type: integer
+   *                 description: The Newsletter's ID to update.
+   *                 example: 1
+   *               name:
+   *                 type: string
+   *                 description: The Newsletter's name.
+   *                 example: My-Newsletter
+   *             required:
+   *               - id
+   *               - name
    *     responses:
    *       200:
-   *         description: API has created succesfully the Newsletter
+   *         description: API has updated succesfully the Newsletter
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 rowsUpdated:
+   *                   type: integer
+   *                   description: The number of newsletters updated, normally must be 1 it may be 0 if it didn't updated.
+   *                   example: 1
+   *
+   *       400:
+   *         description: An error has ocurred
+   *         content:
+   *           text/plain; charset=utf-8:
+   *             schema:
+   *               type: string
+   *               example: name is invalid
    */
   update: async (req, res) => {
     const { id, name } = req.body;
