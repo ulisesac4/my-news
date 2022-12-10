ba28530f7fdbbfa80e7932763e0b7f66800b76ca
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-10 15:25:42 -0600

    add refresh issues at destroy them

--- backend/services/issues/destroy.js
+++ backend/services/issues/destroy.js
@@ -1,6 +1,18 @@
 const Models = require("../../models");
+const hydratePending = require("./hydratePending");
 const Issue = Models.Issue;
 
 module.exports = async (id) => {
-  return Issue.destroy({ where: { id } });
+  const destroy = Issue.destroy({ where: { id } });
+  hydratePending()
+    .then((success) => {
+      console.log("Have correctly hydrated unsent issues at issue destroy");
+    })
+    .catch((err) => {
+      console.error(
+        "[Err] Something happened while hydrating pending issues",
+        err
+      );
+    });
+  return destroy;
 };
