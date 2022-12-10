caaa4380e4cfdcd88316c4b566fe8c3bf2f2b256
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-09 22:00:35 -0600

    Add stub for controllers

---
+++ backend/controllers/index.js
@@ -0,0 +1,6 @@
+const Issues = require("./issues");
+const Newsletters = require("./newsletters");
+const Recipients = require("./recipients");
+const Templates = require("./templates");
+
+module.exports = { Issues, Newsletters, Recipients, Templates };
