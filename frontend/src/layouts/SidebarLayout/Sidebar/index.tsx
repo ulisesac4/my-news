ed17e353f35f2d672bd6126f1f8f7cf018b90d47
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-13 20:42:35 -0600

    remove more unnecesary elements from the sidebar

--- frontend/src/layouts/SidebarLayout/Sidebar/index.tsx
+++ frontend/src/layouts/SidebarLayout/Sidebar/index.tsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react';
-import Scrollbar from 'src/components/Scrollbar';
-import { SidebarContext } from 'src/contexts/SidebarContext';
+import { useContext } from "react";
+import Scrollbar from "src/components/Scrollbar";
+import { SidebarContext } from "src/contexts/SidebarContext";
 
 import {
   Box,
@@ -12,11 +12,11 @@ import {
   Button,
   lighten,
   darken,
-  Tooltip
-} from '@mui/material';
+  Tooltip,
+} from "@mui/material";
 
-import SidebarMenu from './SidebarMenu';
-import Logo from 'src/components/LogoSign';
+import SidebarMenu from "./SidebarMenu";
+import Logo from "src/components/LogoSign";
 
 const SidebarWrapper = styled(Box)(
   ({ theme }) => `
@@ -40,18 +40,18 @@ function Sidebar() {
       <SidebarWrapper
         sx={{
           display: {
-            xs: 'none',
-            lg: 'inline-block'
+            xs: "none",
+            lg: "inline-block",
           },
-          position: 'fixed',
+          position: "fixed",
           left: 0,
           top: 0,
           background:
-            theme.palette.mode === 'dark'
+            theme.palette.mode === "dark"
               ? alpha(lighten(theme.header.background, 0.1), 0.5)
               : darken(theme.colors.alpha.black[100], 0.5),
           boxShadow:
-            theme.palette.mode === 'dark' ? theme.sidebar.boxShadow : 'none'
+            theme.palette.mode === "dark" ? theme.sidebar.boxShadow : "none",
         }}
       >
         <Scrollbar>
@@ -59,7 +59,7 @@ function Sidebar() {
             <Box
               mx={2}
               sx={{
-                width: 52
+                width: 52,
               }}
             >
               <Logo />
@@ -69,35 +69,17 @@ function Sidebar() {
             sx={{
               mt: theme.spacing(3),
               mx: theme.spacing(2),
-              background: theme.colors.alpha.trueWhite[10]
+              background: theme.colors.alpha.trueWhite[10],
             }}
           />
           <SidebarMenu />
         </Scrollbar>
-        <Divider
-          sx={{
-            background: theme.colors.alpha.trueWhite[10]
-          }}
-        />
-        <Box p={2}>
-          <Button
-            href="https://bloomui.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            variant="contained"
-            color="warning"
-            size="small"
-            fullWidth
-          >
-            Upgrade to PRO
-          </Button>
-        </Box>
       </SidebarWrapper>
       <Drawer
         sx={{
-          boxShadow: `${theme.sidebar.boxShadow}`
+          boxShadow: `${theme.sidebar.boxShadow}`,
         }}
-        anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+        anchor={theme.direction === "rtl" ? "right" : "left"}
         open={sidebarToggle}
         onClose={closeSidebar}
         variant="temporary"
@@ -106,9 +88,9 @@ function Sidebar() {
         <SidebarWrapper
           sx={{
             background:
-              theme.palette.mode === 'dark'
+              theme.palette.mode === "dark"
                 ? theme.colors.alpha.white[100]
-                : darken(theme.colors.alpha.black[100], 0.5)
+                : darken(theme.colors.alpha.black[100], 0.5),
           }}
         >
           <Scrollbar>
@@ -116,7 +98,7 @@ function Sidebar() {
               <Box
                 mx={2}
                 sx={{
-                  width: 52
+                  width: 52,
                 }}
               >
                 <Logo />
@@ -126,7 +108,7 @@ function Sidebar() {
               sx={{
                 mt: theme.spacing(3),
                 mx: theme.spacing(2),
-                background: theme.colors.alpha.trueWhite[10]
+                background: theme.colors.alpha.trueWhite[10],
               }}
             />
             <SidebarMenu />
