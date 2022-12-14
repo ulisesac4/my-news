a14f252b28790f77dbb4faf6597ccef2d8d96e0b
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-13 21:18:35 -0600

    adapt the issue's screen

---
+++ frontend/src/views/issues/createIssueDialog.tsx
@@ -0,0 +1,144 @@
+import AddCircleRounded from "@mui/icons-material/AddCircleRounded";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  TextField,
+} from "@mui/material";
+import { useState, ChangeEvent } from "react";
+import { toast } from "react-toastify";
+import NewsletterSelect from "src/components/NewsletterSelect";
+import { NewsletterApi, RecipientApi } from "src/core/API";
+
+function CreateRecipientDialog({ open, onClose }) {
+  const RecipientsAPI = new RecipientApi();
+  const [recipientName, setRecipientName] = useState("");
+  const [newsletterId, setNewsletterId] = useState(0);
+  const [recipientContent, setRecipientContent] = useState("");
+
+  const cleanElements = () => {
+    setRecipientContent("");
+    setRecipientName("");
+  };
+
+  const createRecipient = async () => {
+    try {
+      if (newsletterId && recipientContent) {
+        const recipients = await RecipientsAPI.recipientsPost({
+          emails: recipientContent.split(" ").map((emailElement) => {
+            return emailElement;
+          }),
+          newsletterId: newsletterId.toString(),
+        });
+        if (recipients.status === 200) {
+          toast("Recipient have been created successfully");
+          onClose();
+        } else {
+          console.log("error", recipients.statusText);
+          toast("There was server problem while creating your Recipient");
+        }
+      } else {
+        toast("Emails and Target Newsletter must not be empty");
+      }
+    } catch (error) {
+      console.log("error", error);
+      toast("An error happended while creating the Recipient's name");
+    } finally {
+      cleanElements();
+    }
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={() => {
+        cleanElements();
+        onClose();
+      }}
+    >
+      <DialogTitle>Create Recipients</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          You can write here your new emails separated by a space or load a file
+          with the emails separated by a new line.
+        </DialogContentText>
+
+        <div>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="content"
+            label="Your new Recipients"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(event) => {
+              setRecipientContent(event.target.value);
+            }}
+            value={recipientContent}
+            InputProps={{
+              endAdornment: (
+                <IconButton
+                  color="primary"
+                  aria-label="upload picture"
+                  component="label"
+                >
+                  <input
+                    hidden
+                    type="file"
+                    onChange={(event) => {
+                      event.preventDefault();
+                      const reader = new FileReader();
+                      reader.onload = async (e) => {
+                        let text = e.target.result.toString();
+                        setRecipientContent(
+                          text
+                            .slice(1, text.length - 1)
+                            .split("\n")
+                            .join(" ")
+                        );
+                      };
+                      reader.readAsText(event.target.files[0]);
+                    }}
+                  />
+                  <AddCircleRounded />
+                </IconButton>
+              ),
+            }}
+          />
+        </div>
+        <br />
+
+        <NewsletterSelect
+          onSelectNewsletter={async (value) => {
+            console.log("when update", value);
+            setNewsletterId(value);
+            //await fetchRecipients();
+          }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => {
+            onClose();
+          }}
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={async () => {
+            await createRecipient();
+          }}
+        >
+          Create
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default CreateRecipientDialog;
---
+++ frontend/src/views/issues/index.tsx
@@ -0,0 +1,170 @@
+import DeleteTwoToneIcon from "@mui/icons-material/DeleteTwoTone";
+import {
+  Box,
+  Card,
+  CardHeader,
+  CircularProgress,
+  Divider,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Tooltip,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import NewsletterSelect from "src/components/NewsletterSelect";
+import PageTemplate from "src/components/PageTemplate";
+import {
+  IssueApi,
+  IssuesNewsletterIdGet200ResponseIssuesInner,
+} from "src/core/API";
+import CreateIssueDialog from "./createIssueDialog";
+
+function Issues() {
+  const [issues, setIssues] = useState<
+    IssuesNewsletterIdGet200ResponseIssuesInner[]
+  >([{ id: 1, createdAt: "", updatedAt: "" }]);
+  const [newsletterId, setNewsletterId] = useState(0);
+
+  const [isCreateOpen, setIsCreateOpen] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const IssuesAPI = new IssueApi();
+
+  const fetchIssues = async () => {
+    try {
+      setIsLoading(true);
+      const issues = await IssuesAPI.issuesNewsletterIdGet(newsletterId);
+      if (issues.status === 200) {
+        setIssues(issues.data.issues);
+      } else {
+        // error message here
+      }
+    } catch (error) {
+      console.log("error", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const openCreateDialog = async () => {
+    setIsCreateOpen(true);
+  };
+
+  const closeCreateDialog = async () => {
+    setIsCreateOpen(false);
+    fetchIssues()
+      .then((success) => {})
+      .catch((err) => {});
+  };
+
+  const destroyIssue = async (id) => {
+    try {
+      setIsLoading(true);
+      const issues = await IssuesAPI.issuesDelete({
+        id,
+      });
+      if (issues.status === 200) {
+      } else {
+        toast("Your Issue have been deleted successfully");
+      }
+      fetchIssues()
+        .then((success) => {})
+        .catch((err) => {});
+    } catch (error) {
+      console.log("error", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchIssues();
+  }, [newsletterId]);
+  return (
+    <PageTemplate
+      headerActionName={"Create Issue"}
+      headerDescription={
+        "This where you check the issues you have per newsletter"
+      }
+      headerTitle={"Issues"}
+      pageTitle={"Issues"}
+      headerAction={() => {
+        openCreateDialog();
+      }}
+    >
+      <Card>
+        <CardHeader
+          action={
+            <Box width={150}>
+              <NewsletterSelect
+                onSelectNewsletter={async (value) => {
+                  console.log("when update", value);
+                  setNewsletterId(value);
+                  await fetchIssues();
+                }}
+              ></NewsletterSelect>
+            </Box>
+          }
+          title="Recent Orders"
+        />
+
+        <Divider />
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell align="center">Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {!isLoading ? (
+                issues.length ? (
+                  issues.map((issue) => {
+                    return (
+                      <TableRow key={issue.id}>
+                        <TableCell>{issue.name}</TableCell>
+                        <TableCell align="center">
+                          <Tooltip title="Delete Issue" arrow>
+                            <IconButton
+                              size="small"
+                              onClick={async () => {
+                                await destroyIssue(issue.id);
+                              }}
+                            >
+                              <DeleteTwoToneIcon fontSize="small" />
+                            </IconButton>
+                          </Tooltip>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={6}>no records found</TableCell>{" "}
+                  </TableRow>
+                )
+              ) : (
+                <CircularProgress />
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Card>
+      <CreateIssueDialog
+        open={isCreateOpen}
+        onClose={() => {
+          closeCreateDialog();
+        }}
+      />
+    </PageTemplate>
+  );
+}
+
+export default Issues;
