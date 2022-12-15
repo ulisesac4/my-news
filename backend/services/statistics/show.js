7aecc1d158b6ca4237aab01f890206d459d70839
ulises avila camargo <ulisesavila04@gmail.com>
2022-12-15 14:56:11 -0600

    finish the dat for statistics

--- backend/services/statistics/show.js
+++ backend/services/statistics/show.js
@@ -1,12 +1,31 @@
+const { getWeek, sub, startOfWeek, endOfWeek } = require("date-fns");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 const Models = require("../../models");
 const Issue = Models.Issue;
+const NewsletterRecipient = Models.NewsletterRecipient;
+
+const countRecipients = async (startOfCurrentWeek) => {
+  const recipients = await NewsletterRecipient.findAll({
+    where: {
+      createdAt: {
+        [Op.gte]: new Date(startOfCurrentWeek),
+        [Op.lte]: new Date(endOfWeek(startOfCurrentWeek)),
+      },
+    },
+  });
+  return recipients.length;
+};
 
 module.exports = async () => {
   const data = {
-    sent: { name: "", date: "" },
+    sent: { name: "To be planned", date: "To be planned" },
     amountSent: 0,
-    unsent: { name: "", date: "" },
+    unsent: { name: "To be planned", date: "To be planned" },
     amountUnset: 0,
+
+    weeks: [],
+    entries: [],
   };
 
   const unsent = await Issue.findAll({
@@ -42,10 +61,37 @@ module.exports = async () => {
   //console.log(sentCount, unsentCount, sentRows);
   data.amountSent = sentCount.length;
   data.amountUnset = unsentCount.length;
-  data.sent.name = sent[0].name;
-  data.sent.date = sent[0].publishDate;
-  data.unsent.name = unsent[0].name;
-  data.unsent.date = unsent[0].publishDate;
+  try {
+    data.sent.name = sent[0].name;
+    data.sent.date = sent[0].publishDate;
+  } catch (error) {}
+  try {
+    data.unsent.name = unsent[0].name;
+    data.unsent.date = unsent[0].publishDate;
+  } catch (error) {}
+
+  const currentDate = new Date();
+  const startOfCurrentWeek = startOfWeek(currentDate);
+  const startOfWeekMinus1 = sub(startOfCurrentWeek, { weeks: 1 });
+  const startOfWeekMinus2 = sub(startOfWeekMinus1, { weeks: 1 });
+  const startOfWeekMinus3 = sub(startOfWeekMinus2, { weeks: 1 });
+  const startOfWeekMinus4 = sub(startOfWeekMinus3, { weeks: 1 });
+
+  data.weeks = [
+    startOfCurrentWeek,
+    startOfWeekMinus1,
+    startOfWeekMinus2,
+    startOfWeekMinus3,
+    startOfWeekMinus4,
+  ];
+
+  data.entries = [
+    await countRecipients(startOfCurrentWeek),
+    await countRecipients(startOfWeekMinus1),
+    await countRecipients(startOfWeekMinus2),
+    await countRecipients(startOfWeekMinus3),
+    await countRecipients(startOfWeekMinus4),
+  ];
 
   return data;
 };
